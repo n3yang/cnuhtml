7a86345803293131ed4155247c9e4ec6b48a5e40
n3yang <n3yang@gmai.com>
2013-12-15 22:47:27 +0800

    update

--- js/jquery.cnuaction.js
+++ js/jquery.cnuaction.js
@@ -1,16 +1,25 @@
 jQuery.cnuAction = {
 
+    getBaseUri: function(){
+        return 'http://115.47.56.228:8080/alumni/service';
+    },
+
+
     login: function (username, password, rememberme){
         $.ajax({
-            url: 'proxy.php?pa=login',
+            // url: 'proxy.php?pa=login',
+            url: 'http://115.47.56.228:8080/alumni/service/login?v=1&cid=1',
             type: 'POST',
-            dataType: 'json',
-            data: {name:username,password:password},
+            contentType: 'application/json',
+            dataType: "json",
+            data: '{name:"' + username + '",password:"' + password + '"}',
         })
-        .done(function(m) {
-            if(m.rc==1){
-                $.cookie('admin', m.admin);
-                $.cookie('sid', m.sid);
+        .done(function(d) {
+            if(d.ec==1 && d.rc==1){
+                if( d.admin == 1 ){ 
+                    $.cookie('admin', d.admin);
+                }
+                $.cookie('sid', d.sid);
                 location.href = './alumnus.html';
             } else {
                 alert('用户名或密码错误');
@@ -21,17 +30,35 @@ jQuery.cnuAction = {
         });
     },
 
-    mediaList: function (type, page, num, preview_len) {
+    mediaList: function (type, page, num, previewLen) {
         sid = $.cookie('sid');
-        console.log(sid);
+        if (!type) type=1;
+        if (!page) {page=1};
+        if (!num) {num=10};
+        if (!previewLen) {previewLen=200};
         $.ajax({
-            url: 'proxy.php?pa=media/list',
-            type: 'POST',
-            dataType: 'json',
-            data: {type:type, page:page, num:num, previewLen:preview_len, sid:sid},
+            // url: 'proxy.php?pa=media/list?v=1&cid=1',
+            url: 'http://115.47.56.228:8080/alumni/service/media/list?v=1&cid=1',
+            type: 'get',
+            dataType: "json",
+            data: {sid:sid, type:type, page:page, num:num, previewLen:previewLen},
         })
         .done(function(d) {
-            console.log(d);
+            if (d.ec==1 && d.rc==1) {
+                // set title
+                if (type==1) {title='校友动态';}
+                else if(type==2) {title='通知公告';}
+                $('.news-wrap .title').text(title);
+                // template render
+                bt=baidu.template;
+                document.getElementById('d-news-list').innerHTML = bt('t:tpl-new-list',d);
+                // $('.news-wrap .news-list').innerHTML = bt('t:_1234-abcd-1',d);
+                // $.each(d.list, function(index, val) {
+                //     $('.news-list ul').
+                // });
+            } else if (d.ec==-1 || ec==-5){
+                alert('操作超时，请重新登陆');
+            }
         })
         .fail(function() {
             alert('connection fail, try later please!');
@@ -39,4 +66,27 @@ jQuery.cnuAction = {
         
     },
 
+    mediaTopList: function (type, page, num, previewLen) {
+        sid = $.cookie('sid');
+        if (!type) type=1;
+        if (!page) {page=1};
+        if (!num) {num=10};
+        if (!previewLen) {previewLen=100};
+        $.ajax({
+            // url: 'proxy.php?pa=media/top/list?v=1&cid=1',
+            url: 'http://115.47.56.228:8080/alumni/service/media/top/list?v=1&cid=1',
+            type: 'get',
+            dataType: "json",
+            data: {sid:sid, type:type, page:page, num:num, previewLen:previewLen},
+        })
+        .done(function(d) {
+            if (d.ec==1 && d.rc==1) {
+                // $('.big-banner ul').html($('.big-banner ul').html() + '<li></li>')
+            };
+        })
+        .fail(function() {
+            alert('connection fail, try later please!');
+        });
+        
+    },
 }
--- proxy.php
+++ proxy.php
@@ -8,10 +8,10 @@ $base_uri = 'http://115.47.56.228:8080/alumni/service/';
 // $_POST['previewLen']=100;
 // $_POST['sid'] = 'bb3676615e121d64628926de85bc544637cd46e7c7a095978dba7e45134327d487487d1f9b65940d';
 
-if (!empty($_POST)) {
+if ($_GET['pa']=='login') {
 
 	$request_uri = $base_uri . $_GET['pa'] . '?v=1&cid=1';
-	$data = json_encode($_POST);
+	$data = file_get_contents('php://input');
 	$option = array(
 		CURLOPT_URL				=> $request_uri,
 		CURLOPT_RETURNTRANSFER	=> 1,
@@ -24,4 +24,14 @@ if (!empty($_POST)) {
 	curl_setopt_array($ch, $option);
 	$rs = curl_exec($ch);
 	echo $rs;
+} else {
+	$request_uri = $base_uri . $_GET['pa'];
+	foreach ($_GET as $key => $value) {
+		if ($key=='pa') {
+			continue;
+		}
+		$r.= '&' . $key . '=' . $value;
+	}
+	$request_uri.=$r;
+	echo file_get_contents($request_uri);
 }
