79d9ae0190640958a8a1ea97871b7cb3317b35b7
YiBingWang <e6line@126.com>
2013-12-20 13:13:39 +0800

    update

--- js/jquery.cnuaction.js
+++ js/jquery.cnuaction.js
@@ -1,18 +1,31 @@
 jQuery.cnuAction = {
 
-    getBaseUri: function(){
+    getBaseUrl: function(){
         return 'http://115.47.56.228:8080/alumni/service';
     },
+	
+	convert2HTML: function ( str ){
+		var re = /[\r\n]+/mg;
+		return str.replace( re, "</p><br/><p>");
+	},
 
-
+	getQueryStringByName: function (name){
+		var result = location.search.match(new RegExp("[\?\&]" + name+ "=([^\&]+)","i"));
+		if(result == null || result.length < 1)
+		{
+			return "";
+		}
+		return result[1];
+	}, 
+	
+	//登录
     login: function (username, password, rememberme){
         $.ajax({
-            // url: 'proxy.php?pa=login',
-            url: 'http://115.47.56.228:8080/alumni/service/login?v=1&cid=1',
+            url: $.cnuAction.getBaseUrl() + '/login?v=1&cid=1',
             type: 'POST',
             contentType: 'application/json',
             dataType: "json",
-            data: '{name:"' + username + '",password:"' + password + '"}',
+            data: '{name:"' + username + '",password:"' + password + '"}'
         })
         .done(function(d) {
             if(d.ec==1 && d.rc==1){
@@ -29,7 +42,8 @@ jQuery.cnuAction = {
             alert('connection fail, try later please!');
         });
     },
-
+	
+	//设置普通列表
     mediaList: function (type, page, num, previewLen) {
         sid = $.cookie('sid');
         if (!type) type=1;
@@ -37,11 +51,10 @@ jQuery.cnuAction = {
         if (!num) {num=3};
         if (!previewLen) {previewLen=200};
         $.ajax({
-            // url: 'proxy.php?pa=media/list?v=1&cid=1',
-            url: 'http://115.47.56.228:8080/alumni/service/media/list?v=1&cid=1',
+            url: $.cnuAction.getBaseUrl() + '/media/list?v=1&cid=1',
             type: 'get',
             dataType: "json",
-            data: {sid:sid, type:type, page:page, num:num, previewLen:previewLen},
+            data: {sid:sid, type:type, page:page, num:num, previewLen:previewLen}
         })
         .done(function(d) {
             if (d.ec==1 && d.rc==1) {
@@ -52,8 +65,18 @@ jQuery.cnuAction = {
                 else if(type==4) {title='回馈母校';}
                 $('.news-wrap .title').text(title);
                 // template render
-                bt=baidu.template;
-                document.getElementById('d-news-list').innerHTML = bt('t:tpl-new-list',d);
+                bt = baidu.template;
+                $('#d-news-list').html( bt('t:tpl-new-list',d) );
+				
+				
+				$(".item-text .input-btn").each(function(){
+					$.cnuAction.setDetailHerf($(this));
+				});
+				
+				$(".item-pic a").each(function(){
+					$.cnuAction.setDetailHerf($(this));
+				});
+				
 				
 				$('#a-prev-page').click(function(event) {
 					
@@ -63,18 +86,12 @@ jQuery.cnuAction = {
 					$.cnuAction.mediaList(current_type, $(this).attr('data-page-no'));
 					$(this).attr('data-page-no', $(this).attr('data-page-no')-1);
 					$('#a-prev-page').attr('data-page-no', $('#a-prev-page').attr('data-page-no')-1);
-					console.log($('#a-prev-page').attr('data-page-no'));
 				});
 				$('#a-next-page').click(function(event) {
 					$.cnuAction.mediaList(current_type, $(this).attr('data-page-no'));
 					$(this).attr('data-page-no', $(this).attr('data-page-no')+1);
 					$('#a-prev-page').attr('data-page-no', $('#a-prev-page').attr('data-page-no')+1);
-					console.log($('#a-next-page').attr('data-page-no'));
 				});
-                // $('.news-wrap .news-list').innerHTML = bt('t:_1234-abcd-1',d);
-                // $.each(d.list, function(index, val) {
-                //     $('.news-list ul').
-                // });
             } else if (d.ec==-1 || ec==-5){
                 alert('操作超时，请重新登陆');
 				location.href = './login.html';
@@ -85,7 +102,8 @@ jQuery.cnuAction = {
         });
         
     },
-
+	
+	//设置幻灯
     mediaTopList: function (type, page, num, previewLen) {
         sid = $.cookie('sid');
         if (!type) type=1;
@@ -93,15 +111,28 @@ jQuery.cnuAction = {
         if (!num) {num=10};
         if (!previewLen) {previewLen=100};
         $.ajax({
-            // url: 'proxy.php?pa=media/top/list?v=1&cid=1',
-            url: 'http://115.47.56.228:8080/alumni/service/media/top/list?v=1&cid=1',
+            url: $.cnuAction.getBaseUrl() + '/media/top/list?v=1&cid=1',
             type: 'get',
             dataType: "json",
-            data: {sid:sid, type:type, page:page, num:num, previewLen:previewLen},
+            data: {sid:sid, type:type, page:page, num:num, previewLen:previewLen}
         })
         .done(function(d) {
             if (d.ec==1 && d.rc==1) {
-                // $('.big-banner ul').html($('.big-banner ul').html() + '<li></li>')
+               bt = baidu.template;
+			   $('#b_item').html( bt('t:tpl-banner-list',d) );
+			   
+				$('.flexslider').flexslider();
+				$('.flexslider').hover(function() {
+					$('.flex-direction-nav li a.prev').css('display', 'block');
+					$('.flex-direction-nav li a.next').css('display', 'block');
+				}, function() {
+					$('.flex-direction-nav li a.prev').css('display', 'none');
+					$('.flex-direction-nav li a.next').css('display', 'none');
+				});
+				
+				$(".slides a").each(function(){
+					$.cnuAction.setDetailHerf($(this));
+				});
             };
         })
         .fail(function() {
@@ -109,4 +140,39 @@ jQuery.cnuAction = {
         });
         
     },
+	
+	//设置文章按钮链接
+	setDetailHerf: function(obj){
+		obj.attr("href", './details.html?detailId=' + obj.attr("data-id"));
+	},
+	
+	//设置文章详情
+	mediaDetail: function(){
+		var detailId =  $.cnuAction.getQueryStringByName('detailId');
+		sid = $.cookie('sid');
+		
+        $.ajax({
+            url: $.cnuAction.getBaseUrl() + '/media/'+ detailId +'/detail?v=1&cid=1',
+            type: 'get',
+            dataType: "json",
+            data: {sid:sid}
+        })
+        .done(function(d) {
+			
+            if (d.ec==1 && d.rc==1) {
+				
+               bt = baidu.template;
+			   
+			   $('#details_wrap').html( bt('t:tpl-new-detail',d) );
+			   
+			   $("#detail_content").html($.cnuAction.convert2HTML(d.content))
+			   
+            };
+        })
+        .fail(function() {
+            alert('connection fail, try later please!');
+        });
+		
+	},
+	
 }
